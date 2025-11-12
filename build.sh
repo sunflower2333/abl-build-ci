5f7ba2cedbde0dc2855a8981ca56dff0537eb066
Kancy Joe <54024877+sunflower2333@users.noreply.github.com>
2025-11-13 04:45:34 +0800

    add patch step and modify notes in build script

--- build.sh
+++ build.sh
@@ -7,9 +7,10 @@
 # SDLLVM Download:
 #   https://github.com/SuleMareVientu/Qualcomm_Snapdragon_LLVM_ARM_Toolchain_OEM/releases/download/16.1.3.0/16.1.3.0.zip
 ABL_SRC=$1
-TARGET=$2
+PATCH_DIR=$2
+TARGET=$3
 
-if [ -z "$TARGET" ]; then
+if [ -z "$TARGET" || -z "PATCH_DIR" || -z "TARGET" ]; then
     echo "Usage:"
     echo -e "\t$0  <dir> <codename>"
     echo "Example:"
@@ -25,11 +26,16 @@ SDLLVM_VERSION="10.0"
 
 if [ ! -d "$SDLLVM_PATH" ]; then
     echo "SDLLVM directory not found!"
-    echo "Please download sdllvm from:"
-    echo -e "\thttps://github.com/SuleMareVientu/Qualcomm_Snapdragon_LLVM_ARM_Toolchain_OEM/releases/download/16.1.3.0/16.1.3.0.zip"
-    echo "and extract SDLLVM to $SDLLVM_PATH/$SDLLVM_VERSION"
+    echo "Please update git submodules"
     exit 1
 fi
+
+# Apply Patch
+if [ ! -d $PATCH_DIR ]
+    echo "Patch dir: $PATCH_DIR does not exist"
+    exit 1
+fi
+git am $PATCH_DIR/*
 
 # Build
 cd $ABL_SRC
